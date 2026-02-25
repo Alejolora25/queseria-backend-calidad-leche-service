8a00df4aa9d998568354a3792db7bae64566c3dc
Alejolora25 <alejandroloratovar@outlook.com>
2026-02-24 22:20:04 -0500

    Enable CORS for Vercel domains

--- src/main/java/com/queseria/calidadleche/infrastructure/config/WebFluxCorsConfig.java
+++ src/main/java/com/queseria/calidadleche/infrastructure/config/WebFluxCorsConfig.java
@@ -9,7 +9,9 @@ public class WebFluxCorsConfig implements WebFluxConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**")
-      .allowedOrigins("http://localhost:4200")
+      .allowedOriginPatterns("http://localhost:4200",
+                      "https://queseria-frontend.vercel.app",
+                      "https://*.vercel.app")
       .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
       .allowedHeaders("*");
   }
